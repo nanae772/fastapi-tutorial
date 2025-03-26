d103baa912faac561a4800c3204e0dd9a5452914
nanae <desperado.kazune@gmail.com>
2025-03-26 13:48:47 +0000

    /items/{item_id}エンドポイント実装

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "こんにちわんこそば"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
